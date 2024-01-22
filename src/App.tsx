04d8b14135c1e59cc2e8e037929ab9ab95c0050c
afzal75 <afzalbhuiyan16@gmail.com>
2024-01-22 20:35:48 +0600

    loading animation and button animation done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,63 @@
-import "./App.css"
+// import "./App.css"
+// const App = () => {
+//   return (
+//     <div className="main">
+//       <div className="box-1">
+
+//       </div>
+//       <div className="box-container-2">
+//         <div className="box-2"></div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+
+
+// import "./App.css"
+// const App = () => {
+//   return (
+//     <div className="main">
+//       <button className="btn">Hover Me</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+
+
+
+// import "./Ani.css"
+// const App = () => {
+//   return (
+//     <div className="main">
+//       <div className="box"></div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+import "./Loading.css"
 const App = () => {
   return (
     <div className="main">
-      <div className="box-1">
-
-      </div>
-      <div className="box-container-2">
-        <div className="box-2"></div>
+      <div className="loading">
+        <div className="glass"></div>
+        <div className="box"></div>
       </div>
     </div>
   );
